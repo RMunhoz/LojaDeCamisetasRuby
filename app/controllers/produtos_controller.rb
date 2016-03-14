7f0c9c806dc72095ddc1d57729d8eaafcb6892f9
RMunhoz <rogerio_munhoz@hotmail.com.br>
2016-03-14 11:08:59 -0300

    Funcionando até a busca

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -2,6 +2,23 @@ class ProdutosController < ApplicationController
 
 	def index
 		@produtos_nome = Produto.order(:nome).limit 5
-		@produtos_descricao = Produto.order(:descricao).limit 5
+		@produtos_descricao = Produto.order(:preco).limit 2
 	end	
+
+	def busca
+		@nome_buscar = params[:nome]
+		@produtos = Produto.where "nome like ?", "%#{@nome_buscar}%"
+	end
+
+	def create
+		valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco
+		produto = Produto.create valores
+		redirect_to root_url		
+	end
+
+	def destroy
+		id = params[:id]
+		Produto.destroy id
+		redirect_to root_url
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  root "produtos#index"
+  root	"produtos#index"
+  resources :produtos, only: [:new, :create, :destroy] 
+  get	"/produtos/busca" => "produtos#busca", as: :busca_produto
 
 end
